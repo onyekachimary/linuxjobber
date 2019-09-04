36305585b8bc332fff4cd3661de3b7c08b54c8d4
mary <mary@gmail.com>
2019-09-04 15:20:01 +0100

    first commit

--- wikipol/src/app/app-routing.module.ts
+++ wikipol/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
 	{path: 'profile',component: ProfileComponent}
+	{path: 'home',component: HomeComponent}
 ];
 
 @NgModule({
@@ -12,4 +14,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ProfileComponent]
+export const routingComponents = [ProfileComponent, HomeComponent]
