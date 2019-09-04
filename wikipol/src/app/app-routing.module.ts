c64bc96271121b3f64ec8a63918828bdbe336eca
mary <mary@gmail.com>
2019-09-04 14:58:16 +0100

    first commit

--- wikipol/src/app/app-routing.module.ts
+++ wikipol/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfileComponent } from '../profile/profilecomponent';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
+
 	{path: 'Profile',component: ProfileComponent}
 ];
 
